1b4114c093b525108fdb30dae76abbd2c1f5cbe8
DevRafonalde <rafael.p.albuquerque@gmail.com>
2024-09-19 12:06:07 -0300

    feat: Implementado o controller gRPC para Avaliações

--- app/controller/avaliacoesControllerGRPC.go
+++ app/controller/avaliacoesControllerGRPC.go
@@ -5,7 +5,6 @@ import (
 	"pixelnest/app/configuration/logger"
 	"pixelnest/app/controller/middlewares"
 	"pixelnest/app/service"
-	"strconv"
 
 	pb "pixelnest/app/model/grpc" // Importa o pacote gerado pelos arquivos .proto
 
@@ -39,11 +38,11 @@ func (avaliacoesServer *AvaliacaosServer) FindAllAvaliacoes(context context.Cont
 
 	avaliacoes, erroService := avaliacoesServer.avaliacaoService.FindAllAvaliacoes(context)
 	if erroService.Erro != nil {
-		logger.Logger.Error("Erro ao buscar todas as avaliacoes "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
+		logger.Logger.Error("Erro ao buscar todas as avaliações "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
-	logger.Logger.Info("Buscadas todas as avaliacao",
+	logger.Logger.Info("Buscadas todas as avaliações",
 		zap.Any("usuario", usuarioSolicitante.Usuario),
 	)
 
@@ -51,10 +50,10 @@ func (avaliacoesServer *AvaliacaosServer) FindAllAvaliacoes(context context.Cont
 }
 
 // Função para buscar por uma avaliacao pelo ID
-func (avaliacoesServer *AvaliacaosServer) FindCidadeById(context context.Context, req *pb.RequestId) (*pb.Cidade, error) {
+func (avaliacoesServer *AvaliacaosServer) FindAvaliacaoById(context context.Context, req *pb.RequestId) (*pb.Avaliacao, error) {
 	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-id")
 	if retornoMiddleware.Erro != nil {
-		return &pb.Cidade{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
+		return &pb.Avaliacao{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
 	id := req.GetID()
@@ -62,7 +61,7 @@ func (avaliacoesServer *AvaliacaosServer) FindCidadeById(context context.Context
 		return nil, status.Errorf(codes.InvalidArgument, "ID enviado não é válido ou não foi enviado")
 	}
 
-	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindCidadeById(context, id)
+	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoById(context, id)
 	if erroService.Erro != nil {
 		logger.Logger.Error("Erro ao buscar avaliacao pelo ID "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
@@ -77,76 +76,76 @@ func (avaliacoesServer *AvaliacaosServer) FindCidadeById(context context.Context
 }
 
 // Função para buscar por uma avaliacao pelo nome
-func (avaliacoesServer *AvaliacaosServer) FindCidadeByNome(context context.Context, req *pb.RequestNome) (*pb.Cidade, error) {
-	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-nome")
+func (avaliacoesServer *AvaliacaosServer) FindAvaliacaoByUsuario(context context.Context, req *pb.RequestId) (*pb.ListaAvaliacoes, error) {
+	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-usuario")
 	if retornoMiddleware.Erro != nil {
-		return &pb.Cidade{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
+		return &pb.ListaAvaliacoes{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
-	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindCidadeByNome(context, req.GetNome())
+	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoByUsuario(context, req.GetID())
 	if erroService.Erro != nil {
-		logger.Logger.Error("Erro ao buscar a avaliacao "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
+		logger.Logger.Error("Erro ao buscar as avaliações pelo usuário "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
-	logger.Logger.Info("Buscada uma avaliacao por nome",
+	logger.Logger.Info("Buscadas as avaliações de um usuário",
 		zap.Any("usuario", usuarioSolicitante.Usuario),
 		zap.Any("avaliacao", avaliacao),
 	)
 
-	return avaliacao, nil
+	return &pb.ListaAvaliacoes{Avaliacoes: avaliacao}, nil
 }
 
-// Função para buscar por uma avaliacao pela UF
-func (avaliacoesServer *AvaliacaosServer) FindCidadeByUF(context context.Context, req *pb.RequestUF) (*pb.ListaAvaliacoes, error) {
-	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-uf")
+// Função para buscar por uma avaliacao pelo Produto
+func (avaliacoesServer *AvaliacaosServer) FindAvaliacaoByProduto(context context.Context, req *pb.RequestId) (*pb.ListaAvaliacoes, error) {
+	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-produto")
 	if retornoMiddleware.Erro != nil {
 		return nil, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
-	avaliacoes, erroService := avaliacoesServer.avaliacaoService.FindCidadeByUF(context, req.GetUf())
+	avaliacoes, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoByProduto(context, req.GetID())
 	if erroService.Erro != nil {
-		logger.Logger.Error("Erro ao buscar as avaliacoes pela UF "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
+		logger.Logger.Error("Erro ao buscar as avaliações pelo produto "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
-	logger.Logger.Info("Buscadas as avaliacoes pela UF",
+	logger.Logger.Info("Buscadas as avaliações pelo Produto",
 		zap.Any("usuario", usuarioSolicitante.Usuario),
-		zap.Any("UF", req.GetUf()),
+		zap.Any("produtoId", req.GetID()),
 	)
 
 	return &pb.ListaAvaliacoes{Avaliacoes: avaliacoes}, nil
 }
 
-// Função para buscar por uma avaliacao pela UF
-func (avaliacoesServer *AvaliacaosServer) FindCidadeByCodIbge(context context.Context, req *pb.RequestCodIbge) (*pb.Cidade, error) {
-	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-codIbge")
+// Função para buscar por uma avaliacao pelo Produto
+func (avaliacoesServer *AvaliacaosServer) FindAvaliacaoByJogo(context context.Context, req *pb.RequestId) (*pb.ListaAvaliacoes, error) {
+	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "get-avaliacao-by-jogo")
 	if retornoMiddleware.Erro != nil {
 		return nil, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
-	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindCidadeByCodIbge(context, req.GetCodIbge())
+	avaliacoes, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoByJogo(context, req.GetID())
 	if erroService.Erro != nil {
-		logger.Logger.Error("Erro ao buscar a avaliacao pelo código do IBGE "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
+		logger.Logger.Error("Erro ao buscar as avaliações pelo produto "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
 	logger.Logger.Info("Buscada a avaliacao pelo código do IBGE",
 		zap.Any("usuario", usuarioSolicitante.Usuario),
-		zap.Any("avaliacao", avaliacao),
+		zap.Any("avaliacao", avaliacoes),
 	)
 
-	return avaliacao, nil
+	return &pb.ListaAvaliacoes{Avaliacoes: avaliacoes}, nil
 }
 
 // Função para criar uma nova avaliacao
-func (avaliacoesServer *AvaliacaosServer) CreateCidade(context context.Context, req *pb.Cidade) (*pb.Cidade, error) {
+func (avaliacoesServer *AvaliacaosServer) CreateAvaliacao(context context.Context, req *pb.Avaliacao) (*pb.Avaliacao, error) {
 	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "post-create-avaliacao")
 	if retornoMiddleware.Erro != nil {
-		return &pb.Cidade{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
+		return &pb.Avaliacao{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
-	avaliacaoCriada, erroService := avaliacoesServer.avaliacaoService.CreateCidade(context, req)
+	avaliacaoCriada, erroService := avaliacoesServer.avaliacaoService.CreateAvaliacao(context, req)
 	if erroService.Erro != nil {
 		logger.Logger.Error("Erro ao criar a avaliacao "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
@@ -160,58 +159,20 @@ func (avaliacoesServer *AvaliacaosServer) CreateCidade(context context.Context, 
 	return avaliacaoCriada, nil
 }
 
-// Função para criar avaliacoes em lote a partir de um arquivo csv
-func (avaliacoesServer *AvaliacaosServer) CreateCidadeCSV(context context.Context, req *pb.ListaAvaliacoes) (*pb.RetornoCSV, error) {
-	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "post-create-avaliacao-csv")
-	if retornoMiddleware.Erro != nil {
-		return &pb.RetornoCSV{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
-	}
-
-	var response pb.RetornoCSV
-	for i, avaliacao := range req.GetAvaliacoes() {
-		// Converte o índice em string
-		iString := strconv.Itoa(i)
-
-		avaliacaoCriada, erroService := avaliacoesServer.avaliacaoService.CreateCidade(context, avaliacao)
-		if erroService.Erro != nil {
-			logger.Logger.Error("Erro ao criar a avaliacao "+erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
-			response.Status = append(response.Status, &pb.CSVStatus{
-				Linha:    iString,
-				Mensagem: erroService.Erro.Error(),
-				Status:   int32(erroService.Status),
-			})
-			continue
-		}
-
-		logger.Logger.Info("Criada uma nova avaliacao",
-			zap.Any("usuario", usuarioSolicitante.Usuario),
-			zap.Any("avaliacao", avaliacaoCriada),
-		)
-
-		response.Status = append(response.Status, &pb.CSVStatus{
-			Linha:    iString,
-			Mensagem: "Sucesso",
-			Status:   int32(codes.OK),
-		})
-	}
-
-	return &response, nil
-}
-
 // Função para atualizar uma avaliacao já existente no banco
-func (avaliacoesServer *AvaliacaosServer) UpdateCidade(context context.Context, avaliacao *pb.Cidade) (*pb.Cidade, error) {
+func (avaliacoesServer *AvaliacaosServer) UpdateAvaliacao(context context.Context, avaliacao *pb.Avaliacao) (*pb.Avaliacao, error) {
 	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "put-update-avaliacao")
 	if retornoMiddleware.Erro != nil {
-		return &pb.Cidade{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
+		return &pb.Avaliacao{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
 	}
 
-	avaliacaoAntiga, erroService := avaliacoesServer.avaliacaoService.FindCidadeById(context, avaliacao.GetID())
+	avaliacaoAntiga, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoById(context, avaliacao.GetID())
 	if erroService.Erro != nil {
 		logger.Logger.Error(erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
-	avaliacaoAtualizada, erroService := avaliacoesServer.avaliacaoService.UpdateCidade(context, avaliacao, avaliacaoAntiga)
+	avaliacaoAtualizada, erroService := avaliacoesServer.avaliacaoService.UpdateAvaliacao(context, avaliacao, avaliacaoAntiga)
 	if erroService.Erro != nil {
 		logger.Logger.Error(erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
@@ -227,7 +188,7 @@ func (avaliacoesServer *AvaliacaosServer) UpdateCidade(context context.Context, 
 }
 
 // Função para deletar uma avaliacao existente no banco
-func (avaliacoesServer *AvaliacaosServer) DeleteCidade(context context.Context, req *pb.RequestId) (*pb.ResponseBool, error) {
+func (avaliacoesServer *AvaliacaosServer) DeleteAvaliacao(context context.Context, req *pb.RequestId) (*pb.ResponseBool, error) {
 	usuarioSolicitante, retornoMiddleware := avaliacoesServer.permissaoMiddleware.PermissaoMiddleware(context, "delete-avaliacao-by-id")
 	if retornoMiddleware.Erro != nil {
 		return &pb.ResponseBool{}, status.Errorf(retornoMiddleware.Status, retornoMiddleware.Erro.Error())
@@ -238,13 +199,13 @@ func (avaliacoesServer *AvaliacaosServer) DeleteCidade(context context.Context, 
 		return &pb.ResponseBool{Alterado: false}, status.Errorf(codes.InvalidArgument, "ID enviado não é válido ou não foi enviado")
 	}
 
-	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindCidadeById(context, id)
+	avaliacao, erroService := avaliacoesServer.avaliacaoService.FindAvaliacaoById(context, id)
 	if erroService.Erro != nil {
 		logger.Logger.Error(erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return nil, status.Errorf(erroService.Status, erroService.Erro.Error())
 	}
 
-	deletado, erroService := avaliacoesServer.avaliacaoService.DeleteCidadeById(context, id)
+	deletado, erroService := avaliacoesServer.avaliacaoService.DeleteAvaliacaoById(context, id)
 	if erroService.Erro != nil {
 		logger.Logger.Error(erroService.Erro.Error(), zap.NamedError("err", erroService.Erro), zap.Any("usuario", usuarioSolicitante.Usuario))
 		return &pb.ResponseBool{Alterado: false}, status.Errorf(erroService.Status, erroService.Erro.Error())
